e1ddeb915c98f6457c4e15d09c783fc6b5377ac7
myjeong19 <myjeong19@naver.com>
2025-01-08 20:37:39 +0900

    🔧 Update configuartion babel

---
+++ src/tsx/component.tsx
@@ -0,0 +1,14 @@
+export const Component = () => {
+  return (
+    <main>
+      <h1>Main</h1>
+      <button>Button</button>
+      <section>
+        <strong>Section</strong>
+        <article>
+          <p>Article</p>
+        </article>
+      </section>
+    </main>
+  );
+};
