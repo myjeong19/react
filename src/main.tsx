410f755af6e0661439b9a6c221a20ddaf3668ecb
myjeong19 <myjeong19@naver.com>
2025-01-10 03:00:50 +0900

    ➖ Remove React

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { Component } from './tsx';
-
-const appElement = document.getElementById('app');
-const root = createRoot(appElement!);
-
-root.render(<Component />);
