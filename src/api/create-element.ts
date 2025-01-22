a3fdd7e647c810bb641171e1d82635619b3daeda
myjeong19 <myjeong19@naver.com>
2025-01-22 18:24:46 +0900

    ♻️ Refactor createElement Function

--- src/api/create-element.ts
+++ src/api/create-element.ts
@@ -1,30 +1,30 @@
 type Element = keyof HTMLElementTagNameMap;
-type Component = (props: InputProps | null, ...children: unknown[]) => CreateElementResult;
-type Type = Element | Component | 'fragment';
+type Component = (props: VirtualDOMInputProps | null, ...children: unknown[]) => VirtualDOM;
+type CreateType = Element | Component | 'fragment';
 
-type InputProps = {
+export type VirtualDOMInputProps = {
   key?: string | null;
   ref?: unknown | null;
   [key: string]: unknown;
 };
 
-type OutputProps = {
+export type VirtualDOMProps = {
   [key: string]: unknown;
   children?: unknown | unknown[];
 };
 
-type CreateElementResult = {
-  type?: Type;
-  props: OutputProps;
+export type VirtualDOM = {
+  type?: CreateType;
+  props: VirtualDOMProps;
   key?: string | null;
   ref?: unknown | null;
 };
 
 export const createElement = (
-  type: Type,
-  props: InputProps | null,
+  type: CreateType,
+  props: VirtualDOMInputProps | null,
   ...children: unknown[]
-): CreateElementResult => {
+): VirtualDOM => {
   const { key, ref, ...restProps } = props || {};
 
   if (typeof type === 'function') {
