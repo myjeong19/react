e1f8d2fe5f7e2d7ca69dfcf28bde1fa9bc6a4526
myjeong19 <myjeong19@naver.com>
2025-01-25 06:07:03 +0900

    ♻️ Refactor file path

--- src/dom/client/create-root.ts
+++ src/dom/client/create-root.ts
@@ -1,4 +1,4 @@
-import type { VirtualDOM } from '@src/api/create-element';
+import type { VirtualDOM } from '@src/api';
 import { render as callRender } from '@src/util';
 
 export const createRoot = (rootElement: HTMLElement) => {
